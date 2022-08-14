17f0f646f7a119f5c1ab7fbff496868449eaa29c
resloved <bfriesenwork@gmail.com>
2022-08-14 13:17:22 -0700

    Replaced projectile and reformatted file structure

--- init.el
+++ init.el
@@ -1,1 +1,23 @@
-(org-babel-load-file "~/.emacs.d/README.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode nil nil (cua-base))
+ '(fringe-mode 0 nil (fringe))
+ '(helm-minibuffer-history-key "M-p")
+ '(menu-bar-mode nil)
+ '(org-modules
+   '(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-tempo))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil)
+ '(uniquify-buffer-name-style nil nil (uniquify)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:font "Monaco" :height 120))))
+ '(tree-sitter-hl-face:property ((t (:inherit font-lock-constant-face)))))
+(org-babel-load-file "~/.emacs.d/config.org")
