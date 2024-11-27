8e3f873103d8cf6a9821d55f884739d48a4cce09
resloved <bfriesen95@gmail.com>
2024-11-27 11:19:18 -0800

    changing to work on windows

--- init.el
+++ init.el
@@ -13,11 +13,4 @@
  '(tool-bar-mode nil)
  '(tooltip-mode nil)
  '(uniquify-buffer-name-style nil nil (uniquify)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:font "Monaco" :height 120))))
- '(tree-sitter-hl-face:property ((t (:inherit font-lock-constant-face)))))
 (org-babel-load-file "~/.emacs.d/config.org")
